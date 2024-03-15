4d8df22066ffce327127e093806989134897f5a9
Chantal Ariu <chantalariu227@gmail.com>
2024-03-15 16:52:42 +0100

    Fix formatting for assignment 5 framework

--- a5-diff/a5-diff-frame.c
+++ a5-diff/a5-diff-frame.c
@@ -15,7 +15,8 @@
 
 int main(int argc, char **argv)
 {
-	if (argc < 3) {
+	if (argc < 3)
+	{
 		fprintf(stderr, "Usage: a5-diff file1 file2 -flags\n");
 		return -1;
 	}
@@ -26,14 +27,16 @@ int main(int argc, char **argv)
 	bool flag_i = false;
 	bool flag_B = false;
 
-    FILE *file_a = NULL;
-    FILE *file_b = NULL;
+	FILE *file_a = NULL;
+	FILE *file_b = NULL;
 
-    char *str_a = NULL;
+	char *str_a = NULL;
 	char *str_b = NULL;
 
-	while ((option = getopt(argc - 2, argv + 2, "iB")) != -1) {
-		switch (option) {
+	while ((option = getopt(argc - 2, argv + 2, "iB")) != -1)
+	{
+		switch (option)
+		{
 		case 'i':
 			flag_i = true;
 			break;
@@ -45,37 +48,39 @@ int main(int argc, char **argv)
 		}
 	}
 
-    /* open files to check existence */
+	/* open files to check existence */
 	file_a = fopen(argv[INDX_A], "r");
-	if (file_a == NULL) {
+	if (file_a == NULL)
+	{
 		fprintf(stderr, STR_E " could not open file \"%s\".\n", argv[INDX_A]);
 		goto err_cleanup;
 	}
 
-    file_b = fopen(argv[INDX_B], "r");
-	if (file_b == NULL) {
+	file_b = fopen(argv[INDX_B], "r");
+	if (file_b == NULL)
+	{
 		fprintf(stderr, STR_E " could not open file \"%s\".\n", argv[INDX_B]);
 		goto err_cleanup;
 	}
 
-    fclose(file_a);
-    file_a = NULL;
-    fclose(file_b);
-    file_b = NULL;
+	fclose(file_a);
+	file_a = NULL;
+	fclose(file_b);
+	file_b = NULL;
 
 	fprintf(stderr, "calling my_diff(%s, %s, i = %s, B = %s);\n", argv[INDX_A], argv[INDX_B],
-		BOOL_TO_STR(flag_i), BOOL_TO_STR(flag_B));
+			BOOL_TO_STR(flag_i), BOOL_TO_STR(flag_B));
 
 	ret = my_diff(argv[INDX_A], argv[INDX_B], flag_i, flag_B);
 
 cleanup:
-    if (file_a)
-        fclose(file_a);
-    if (file_b)
-        fclose(file_b);
-	
-    free(str_a);
-    free(str_b);
+	if (file_a)
+		fclose(file_a);
+	if (file_b)
+		fclose(file_b);
+
+	free(str_a);
+	free(str_b);
 
 	return ret;
 
